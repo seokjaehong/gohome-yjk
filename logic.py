431e113319436e7165ed6e6c670c59c720d5b321
seokjae hong <hsj2334@algorithmlabs.co.kr>
2020-05-28 11:50:12 +0900

    gitignore add

---
+++ logic.py
@@ -0,0 +1,14 @@
+from constant import OUT_URL
+
+testcase_list = {
+    'ExampleTD_SQ1_1':
+        {
+            'current_url': 'http://svy.web-survey.co.kr/sp2/S11430vprxsa/svyJ003.asp',
+            'result': ': SQ1=1'
+        },
+    'ExampleTD_SQ1_2':
+        {
+            'current_url': 'http://svy.web-survey.co.kr/sp2/S11430vprxsa/svyJ004.asp',
+            'result': ': SQ1=2'
+        }
+}
